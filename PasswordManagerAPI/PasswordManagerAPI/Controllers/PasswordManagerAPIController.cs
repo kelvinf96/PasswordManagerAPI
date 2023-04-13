365ee569785e30490c1eadb8727eae09888acf45
Kelvin Forde <kelvinforde100@gmail.com>
2023-04-13 20:45:19 +0100

    Added some endpoints for getting all passwords in database, getting a specific users passwords, getting a single password for a user and creating a password

--- PasswordManagerAPI/PasswordManagerAPI/Controllers/PasswordManagerAPIController.cs
+++ PasswordManagerAPI/PasswordManagerAPI/Controllers/PasswordManagerAPIController.cs
@@ -9,12 +9,28 @@ namespace PasswordManagerAPI.Controllers
     [ApiController]
     public class PasswordManagerAPIController : ControllerBase
     {
+        
         // simply for testing purpose, this will be swapped out for a cloud db
         private List<User> users = new List<User>()
         {
                     new User ("0896154071"),
                     new User ("0812345678"),
 
+        };
+
+        // Create two fake user Ids for testing purpose only
+        public static Guid user1Id = System.Guid.NewGuid();
+        public static Guid user2Id = Guid.NewGuid();
+        
+        
+        
+
+        private List<Password> passwords = new List<Password>()
+        {
+            // The user1 and user2 id's be swapped with the user who is creating the passwords ID
+            new Password(user1Id, "Netflix", "TestPassword1"),
+            new Password(user1Id, "SkyGo", "ManUrSh1t"),
+            new Password(user2Id, "Youtube", "T3stPass2"),
         };
 
         //just to test encryption on PasswordService
@@ -68,17 +84,89 @@ namespace PasswordManagerAPI.Controllers
             {
                 User newUser = new User(newUserPhone);
                 users.Add(newUser);
+                // This can be removed at later date, purely for purpose of ensuring user is added
                 foreach( var u  in users)
                 {
                     Console.WriteLine(u.PhoneId);
-                }                                    
-                // create http response with Created status code and listing serialised as content and Location header set to URI for new resource
+                }
+                
+                
                 return CreatedAtRoute("GetUser", new { phone = newUser.PhoneId }, newUser);
             }
             else
             {
-                return BadRequest("Phone number already in use");      // 400, already exists
+                return BadRequest("Phone number already in use");     
             }
         }
+
+        //
+        // Controllers for Passwords
+        //
+
+        // GET allPasswords
+        [HttpGet("getAllPasswords")]
+        public IEnumerable<Password> GetAllPasswords()
+        {
+            return passwords.OrderBy(p => p.PasswordName);
+        }
+
+        // GET api/getUserPasswords/userid 
+        [HttpGet("getUserPasswords/{user}", Name = "GetUserPasswords")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Password> GetUserPasswords(Guid user)
+        {
+            // Find all passwords for that user
+            List<Password> requestedPasswords = passwords.FindAll(p => p.UserId == user);
+            if (requestedPasswords == null)
+            {
+                return NotFound();
+            }
+            return Ok(requestedPasswords);
+        }
+
+        // GET api/getUserSpecificPassword/userid/passwordname
+        [HttpGet("getUserSpecificPassword/{user}/{passwordname}", Name = "GetUserSpecificPassword")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Password> GetUserSpecificPasswords(Guid user, String passwordname)
+        {
+            // Find the specific password for user, ex. get Kelvins Netflix password
+            Password requestedPassword = passwords.SingleOrDefault(p => p.UserId == user && p.PasswordName == passwordname);
+            if (requestedPassword == null)
+            {
+                return NotFound();
+            }
+            return Ok(requestedPassword);
+        }
+
+        // POST api/addPassword
+        [HttpPost("addPassword")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult AddNewPassword([FromBody] Guid user, String passwordname, String passwordvalue)
+        {
+
+            // check if the user already has a password for this eg. Kelvin already has a password for Netflix
+            var record = passwords.SingleOrDefault(p => p.UserId == user && p.PasswordName == passwordname);
+            if (record == null)
+            {
+                Password newPassword = new Password(user, passwordname, passwordvalue);
+                passwords.Add(newPassword);
+                // This can be removed at later date, purely for purpose of ensuring user is added
+                foreach (var p in passwords)
+                {
+                    Console.WriteLine($"{p.UserId}'s password for {p.PasswordName} is {p.PasswordValue}.");
+                }
+
+                
+                return CreatedAtRoute("GetUserSpecificPassword", new { user = newPassword.UserId, passwordname = newPassword.PasswordName }, newPassword);
+            }
+            else
+            {
+                return BadRequest($"You already have a password for {passwordname}");      
+            }
+        }
+
     }
 }
